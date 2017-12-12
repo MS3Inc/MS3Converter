989717c45f29150ead226cd5cad58b378ff7e16e
kokyhin <kokyhin@gmail.com>
2017-12-12 11:07:55 +0200

    Swagger 2.0 settings to MS3

--- src/oas/oas-to-ms3/oas-20-to-ms3/index.ts
+++ src/oas/oas-to-ms3/oas-20-to-ms3/index.ts
@@ -14,16 +14,31 @@ class MS3toOAS20toMS3 {
     this.ms3API = {
       entityTypeName: 'api',
       ms3_version: '1.0',
-      settings: {
-        title: 'title',
-        baseUri: 'baseuri'
-      }
+      settings: this.convertSettings()
     };
-
     return this.ms3API;
+  }
+
+  private getBaseUri() {
+    const host = this.oasAPI.host ? this.oasAPI.host : 'default.com';
+    const http = (this.oasAPI.schemes && this.oasAPI.schemes[0]) ? this.oasAPI.schemes[0] : 'http';
+    const basePath = this.oasAPI.basePath ? this.oasAPI.basePath : '';
+    return `${http}://${host}${basePath}`;
+  }
+
+  private convertSettings() {
+    const info = this.oasAPI.info;
+    const settings: MS3Interface.Settings = {
+      title: info.title,
+      baseUri: this.getBaseUri(),
+      version: info.version,
+      protocols: this.oasAPI.schemes
+    };
+    if (info.description) settings.description = info.description;
+    return settings;
   }
 }
 
-export default function convertOAS30toMS3(oasAPI: OAS20Interface.API): any {
+export default function convertOAS20toMS3(oasAPI: OAS20Interface.API): any {
   return MS3toOAS20toMS3.create(oasAPI).convert();
 }
