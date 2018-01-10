d50d651375d88a26bfee25d861be56f8bfec4142
kokyhin <kokyhin@gmail.com>
2018-01-10 17:14:13 +0200

    Add import swagger func

--- src/oas/index.ts
+++ src/oas/index.ts
@@ -1,4 +1,5 @@
 import * as oas30Interface from './oas-30-api-interface';
 import * as oas20Interface from './oas-20-api-interface';
+import loader from './loader';
 
-export { oas30Interface, oas20Interface };
+export { oas30Interface, oas20Interface, loader };
