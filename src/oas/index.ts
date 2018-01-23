90f98800ff9b7716655ed5ec84447a877128a976
kokyhin <kokyhin@gmail.com>
2018-01-23 09:32:00 +0200

    Add OAS import

--- src/oas/index.ts
+++ src/oas/index.ts
@@ -1,5 +1,6 @@
 import * as oas30Interface from './oas-30-api-interface';
 import * as oas20Interface from './oas-20-api-interface';
+import convertToMs3 from './oas-to-ms3/index';
 import loader from './loader';
 
-export { oas30Interface, oas20Interface, loader };
+export { oas30Interface, oas20Interface, loader, convertToMs3 };
